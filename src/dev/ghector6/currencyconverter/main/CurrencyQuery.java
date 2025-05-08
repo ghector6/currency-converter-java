0ee5156f1f7712ad1e6da577187fad7f50a741b1
Hector Garcia Rosales <ghectormus@gmail.com>
2025-05-08 15:42:20 -0600

    add: CurrencyQuery class with search method

--- src/dev/ghector6/currencyconverter/main/CurrencyQuery.java
+++ src/dev/ghector6/currencyconverter/main/CurrencyQuery.java
@@ -1,4 +1,30 @@
 package dev.ghector6.currencyconverter.main;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CurrencyQuery {
+    public Currency searchCurrency(){
+        URI uri = URI.create("https://v6.exchangerate-api.com/v6/02058015880f09d24d711dab/latest/USD");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            Gson gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+            return gson.fromJson(response.body(), Currency.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
