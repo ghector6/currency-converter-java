16b97db5aa69fcc9b798c4f78538184c52452932
Hector Garcia Rosales <ghectormus@gmail.com>
2025-05-12 16:18:42 -0600

    proto main class

--- src/dev/ghector6/currencyconverter/main/Main.java
+++ src/dev/ghector6/currencyconverter/main/Main.java
@@ -1,13 +1,40 @@
 package dev.ghector6.currencyconverter.main;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         CurrencyQuery currencyQuery = new CurrencyQuery();
         Currency currency = currencyQuery.searchCurrency();
         CurrencyConversion currencyConversion = new CurrencyConversion();
-        System.out.println(currencyConversion.currencyConversion(0, currency));
+        Scanner scan = new Scanner(System.in);
+        double result = 0.0;
+        double quantity;
+        int inputNumber = 0;
+        while (inputNumber != 7) {
+            System.out.println("********************************");
+            System.out.println("Welcome to currency converter! ");
+            System.out.println("Choose an option");
+            System.out.println("1) USD => MXN ");
+            System.out.println("2) MXN => USD ");
+            System.out.println("3) EUR => MXN ");
+            System.out.println("4) MXN => EUR ");
+            System.out.println("5) CAD => MXN ");
+            System.out.println("6) MXN => CAD ");
+            System.out.println("7) Exit: ");
+            System.out.println("********************************\n");
+            inputNumber = scan.nextInt();
+            if(inputNumber == 7){
+                System.out.println("Thanks, see you next time");
+                break;
+            }else{
+                System.out.println("Type amount to convert");
+                quantity = scan.nextDouble();
+                result = currencyConversion.currencyConversion(inputNumber, currency, quantity);
+                System.out.println(result);
+
+            }
+        }
     }
 
 }
