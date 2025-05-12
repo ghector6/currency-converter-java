8addd5148e4c4cc0b3ef4a8e464a562ed30b50c5
Hector Garcia Rosales <ghectormus@gmail.com>
2025-05-12 16:14:59 -0600

    refac: CurrencyConversion with options need to refac

--- src/dev/ghector6/currencyconverter/main/CurrencyConversion.java
+++ src/dev/ghector6/currencyconverter/main/CurrencyConversion.java
@@ -1,13 +1,34 @@
 package dev.ghector6.currencyconverter.main;
 
 public class CurrencyConversion {
-    double currencyConversion(int option, Currency currency) {
-        double result = 0.0;
 
-        if (option == 0) {
+    double currencyConversion(int option, Currency currency, double quantity) {
+
+        double result = 0.0;
+        if (option == 1) {
             double baseCurrency = currency.conversionRates().get("USD");
             double toCurrency = currency.conversionRates().get("MXN");
-            result = baseCurrency * toCurrency;
+            result = (quantity * toCurrency) / baseCurrency;
+        }else if(option == 2){
+            double baseCurrency = currency.conversionRates().get("MXN");
+            double toCurrency = currency.conversionRates().get("USD");
+            result = (quantity * toCurrency) / baseCurrency;
+        }else if(option == 3){
+            double baseCurrency = currency.conversionRates().get("EUR");
+            double toCurrency = currency.conversionRates().get("MXN");
+            result = (quantity * toCurrency) / baseCurrency;
+        }else if(option == 4){
+            double baseCurrency = currency.conversionRates().get("MXN");
+            double toCurrency = currency.conversionRates().get("EUR");
+            result = (quantity * toCurrency) / baseCurrency;
+        }else if(option == 5){
+            double baseCurrency = currency.conversionRates().get("CAD");
+            double toCurrency = currency.conversionRates().get("MXN");
+            result = (quantity * toCurrency) / baseCurrency;
+        }else if(option == 6){
+            double baseCurrency = currency.conversionRates().get("MXN");
+            double toCurrency = currency.conversionRates().get("CAD");
+            result = (quantity * toCurrency) / baseCurrency;
         } else {
             throw new IllegalArgumentException("Invalid option: " + option);
         }
