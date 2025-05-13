9e931c543259b426974a3202434403bec09f550a
Hector Garcia Rosales <ghectormus@gmail.com>
2025-05-13 15:30:44 -0600

    add(ui) Class to show the interface ui

---
+++ src/dev/ghector6/currencyconverter/main/CurrencyUI.java
@@ -0,0 +1,69 @@
+package dev.ghector6.currencyconverter.main;
+
+import java.util.Scanner;
+
+public class CurrencyUI {
+    private final Currency currency;
+    private final CurrencyConversion conversion = new CurrencyConversion();
+    private final Scanner scanner = new Scanner(System.in);
+
+    public CurrencyUI(Currency currency){
+        this.currency = currency;
+    }
+
+    public void start(){
+        int inputNumber = 0;
+        while(inputNumber != 7){
+            printMenu();
+            inputNumber = readInt("Choose an option");
+            if(inputNumber == 7){
+                System.out.println("Thanks, see you next time!");
+                break;
+            }
+
+            CurrencyOptions option = CurrencyOptions.fromOption(inputNumber);
+            if(option == null){
+                System.out.println("Invalid option");
+                continue;
+            }
+
+            double amount = readDouble("Enter an amount to convert");
+            double result = conversion.currencyConversion(option, currency, amount);
+            System.out.printf("Result: %.2f %s%n%n", result, option.getTo());
+        }
+
+    }
+    private void printMenu() {
+        System.out.println("*******************************");
+        System.out.println("Welcome to Currency Converter");
+        for (CurrencyOptions opt : CurrencyOptions.values()) {
+            System.out.printf("%d) %s => %s%n", opt.getOption(), opt.getFrom(), opt.getTo());
+        }
+        System.out.println("7) Exit");
+        System.out.println("*******************************");
+    }
+
+    private int readInt(String prompt){
+        while(true){
+            System.out.println(prompt);
+            if(scanner.hasNextInt()){
+                return scanner.nextInt();
+            }
+            System.out.println("Invalid input. Select a number");
+            scanner.nextInt();
+        }
+    }
+
+    private double readDouble(String prompt){
+        while(true){
+            System.out.println(prompt);
+            if(scanner.hasNextDouble()){
+                return scanner.nextDouble();
+            }
+            System.out.println("Invalid input. Select a number");
+            scanner.nextInt();
+        }
+    }
+
+
+}
