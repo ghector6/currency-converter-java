34c4d48de10acb25fd19b9d3e278ff9327a7c8ed
Hector Garcia Rosales <ghectormus@gmail.com>
2025-05-13 14:17:56 -0600

    refac(enum): Creating an Enum class
    Handling options for currencycodes

---
+++ src/dev/ghector6/currencyconverter/main/CurrencyOptions.java
@@ -0,0 +1,33 @@
+package dev.ghector6.currencyconverter.main;
+
+public enum CurrencyOptions {
+    USD_TO_MXN(1, "USD", "MXN"),
+    MXN_TO_USD(2, "MXN", "USD"),
+    EUR_TO_MXN(3, "EUR", "MXN"),
+    MXN_TO_EUR(4, "MXN", "EUR"),
+    CAD_TO_MXN(5, "CAD", "MXN"),
+    MXN_TO_CAD(6, "MXN", "CAD");
+
+    private final int option;
+    private final String from;
+    private final String to;
+
+    CurrencyOptions(int option, String from, String to) {
+        this.option = option;
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getOption() { return option; }
+
+    public String getFrom() { return from; }
+
+    public String getTo() { return to; }
+
+    public static CurrencyOptions fromOption(int option) {
+        for (CurrencyOptions c : values()) {
+            if (c.option == option) return c;
+        }
+        return null;
+    }
+}
